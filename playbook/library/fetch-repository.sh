c0879f38a5fd0ada2a5ff2258da8d5b7aa5badc8
Matthew J. Lenzo <matthew.lenzo@gmail.com>
2021-08-24 23:03:35 -0400

    Use shallow clone

--- playbook/library/fetch-repository.sh
+++ playbook/library/fetch-repository.sh
@@ -4,9 +4,10 @@ REPO=$1
 DEST=$2
 
 if [[ ! -d "${DEST}" ]]; then
-  git clone --mirror --recursive "${REPO}" "${DEST}"
+  git clone --mirror --depth=1 --quiet "${REPO}" "${DEST}"
 else
   pushd "${DEST}" &> /dev/null;
-    git remote update
+    git fetch --depth=1 --prune --prune-tags --quiet
+    #git remote update --prune
   popd &> /dev/null;
 fi
